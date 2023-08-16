e1843ac9062e4510188a179caf505479815d2a51
tom <tom.@atguigu.com>
2023-08-16 17:40:52 +0800

    first commit

---
+++ src/main/java/com/atguiug/git.java
@@ -0,0 +1,12 @@
+package com.atguiug;
+
+/**
+ * @auther admin
+ * @create 2023-08-16-17:36
+ */
+public class git {
+
+    public static void main(String[] args) {
+        System.out.println("gittest!!!!!!!!!!!!");
+    }
+}
